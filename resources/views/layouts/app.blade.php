64fc49a47e6da9cc557d637135db3b60c8759b14
marco <marcoaoc83@gmail.com>
2018-03-03 21:23:25 -0400

    master

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
                   <div class="input-group">
                     <input type="text" class="form-control" placeholder="Search for...">
                     <span class="input-group-btn">
-                      <button class="btn btn-default" type="button">Go!</button>
+                      <button class="btn btn-default" type="button">vai!</button>
                     </span>
                   </div>
                 </div>
