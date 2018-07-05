451dab1d5c9537b519ecc18a3f4c1ec19a2c4c16
MarcoCoelho <marcoaoc83@gmail.com>
2018-07-05 17:09:47 -0400

    CARD IMPORTACAO

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Parcela;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
@@ -14,6 +16,18 @@ class AdminController extends Controller
         if($user->funcao==4){
             return view('admin.cidadao.index');
         }
-        return view('admin.home.index');
+        $parcela_qtde = Parcela::count('ParcelaId');
+        $parcela_qtde=number_format($parcela_qtde, 0, '', '.');
+
+        $parcela_qtde_aberta = DB::table('cda_parcela')->where("SitPagId","61")->count();
+        $parcela_qtde_aberta=number_format($parcela_qtde_aberta, 0, '', '.');
+
+        $parcela_sum = Parcela::sum('TotalVr');
+        $parcela_sum=number_format($parcela_sum, 2, ',', '.');
+
+        $parcela_sum_aberta = DB::table('cda_parcela')->where("SitPagId","61")->sum('TotalVr');
+        $parcela_sum_aberta=number_format($parcela_sum_aberta, 2, ',', '.');
+
+        return view('admin.home.index',compact('parcela_qtde','parcela_qtde_aberta','parcela_sum','parcela_sum_aberta'));
     }
 }
